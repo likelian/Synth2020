ff50f5f2f7a1033b0f94a9e9c20ed7fb4903951e
likelian <74072319+likelian@users.noreply.github.com>
2020-11-08 19:40:09 +0000

    Deploying to gh-pages from master @ 6e20bb186032e99c21d506b95a1f9836de9e7b05 🚀

--- fonts.js
+++ fonts.js
@@ -171,7 +171,7 @@ Module['FS_createPath']('/resources', 'fonts', true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}, {"filename": "/resources/fonts/MicrochipRegular-ExWg.ttf", "start": 145348, "end": 168788, "audio": 0}], "remote_package_size": 168788, "package_uuid": "1538bd1c-adbd-4a49-95f6-3cb1287ecd51"});
+   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}, {"filename": "/resources/fonts/MicrochipRegular-ExWg.ttf", "start": 145348, "end": 168788, "audio": 0}], "remote_package_size": 168788, "package_uuid": "8a8217af-8762-49fb-adbe-d9b3b22c2787"});
   
   })();
   
